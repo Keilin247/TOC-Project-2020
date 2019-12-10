1b380778922ff27e4508f41068bba42b429a79b7
keilin chiu <keilin247@gmail.com>
2019-12-11 06:34:44 +0800

    updated

--- fsm.py
+++ fsm.py
@@ -15,6 +15,8 @@ class TocMachine(GraphMachine):
     def is_going_to_income(self, event):
         text = event.message.text
         if (text.split(' ')[0]=="Income:") and (len(text.split(' '))==2):
+            global sid
+            global value
             sid = text.split(' ')[1]
             value = int(sid)
             text = text.split(' ')[0]
